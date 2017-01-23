b00c3d6be0d1e08c8597d0e3f21b1e5dc123115a
Mike Slinn <mslinn@micronauticsresearch.com>
2017-01-22 22:22:25 -0800

    -

--- post-tweet.js
+++ post-tweet.js
@@ -1,17 +1,16 @@
-console.log("Example is up now..")
+console.log("Example is up now.")
 var Twit = require('twit');
 var config = require('./config')
 var T = new Twit(config);
-var tweet = { 
-status: 'hello world!!' }
+var tweet = {
+  status: 'hello world!!'
+}
 
 T.post('statuses/update', tweet, tweeted)
 
- function tweeted(err, data, response) {
-  if(err){
-console.log("Something went wrong!");
-}
-else{
-console.log("Voila It worked!");
-}
+function tweeted(err, data, response) {
+  if (err)
+    console.log("Something went wrong!");
+  else
+    console.log("Voila It worked!");
 }
